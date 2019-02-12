b4c99ae1df6e671a440f1714cc7b43005a62a250
Christian <claygeng@ucsd.edu>
2019-02-11 16:14:14 -0800

    Christian Homepage draft

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var path = require('path');
 var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
+var homepage = require('./routes/homepage(C)');
 // Example route
 // var user = require('./routes/user');
 
@@ -34,7 +35,7 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', index.view);
+app.get('/', homepage.view);
 // Example route
 // app.get('/users', user.list);
 
