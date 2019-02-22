24e7ca5d286635dcbd8c8a43dc3a298f832bdc8c
i0c006@ucsd.edu <i0c006@ucsd.edu>
2019-02-21 21:50:59 -0800

    hi

--- public/js/login.js
+++ public/js/login.js
@@ -1,11 +1,24 @@
 $(document).ready(function () {
 });
 
+function addEvent() {
+	var eventAdded = document.getElementById('eventA').value;
+	var timeAdded = document.getElementById('timeA').value;
+	var placeAdded = document.getElementById('placeA').value;
+	var detailAdded = document.getElementById('detailA').value;
+	$('.toAdd').append('<span><button class="btn btn-secondary dropdown-toggle" type="button" id="newEvent" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">' + eventAdded + ' @ ' + timeAdded + '</button><div class="dropdown-menu container" aria-labelledby="dropdownMenuButton2"> <a class="dropdown-item" href="#">' + placeAdded + '<button type="button" class="btn btn-light float-right edit" data-toggle="modal" data-target="#myModal">Edit</button></a><a class="dropdown-item" href="#">' + detailAdded + '</a><hr><form id="commentForm"><input class="form-control form-control-sm" type="textarea" id="commentAdd" placeholder="add comment" name="commentPlz"><button type="button" class="btn btn-primary commentButton">Comment</button></form></span>'
+          
+		);
+	$('')
+}
+
+
 
 
 function addComment() { 
 	var commentAdded = document.getElementById('commentAdd').value;
 	$('.exampleComments').append('<a class="dropdown-item" href="#">Henry: '+commentAdded+'</a>');
+	event.preventDefault();
 };
 
 $('.loginButton').click(function (req, res) {
@@ -21,6 +34,11 @@ $('.Create').click(function (req, res) {
 	app.get('/planadd', function(req, res){
 	res.render('/planadd');
 });
+});
+
+$('.eventButton').click(function() {
+	addEvent();
+	$('#myModal').modal('hide');
 });
 
 $('.commentButton').click(function() {
